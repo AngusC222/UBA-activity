f8eb45abc646fe579b0a73cf5aab99a050d77cc2
AngusC222 <anguscrighton@icloud.com>
2024-01-22 21:40:46 +0000

    python documentation and github commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# UBA activity monitor
+<center>![Discord: aaa2.](https://img.shields.io/badge/Discord-aaa2.-white?labelColor=blue&style=flat&logo=discord&logoColor=white) [![Roblox: SlRANGUS](https://img.shields.io/badge/Roblox-SlRANGUS-white?labelColor=Grey&style=flat&logo=roblox&logoColor=white&link=https://www.roblox.com/users/557148823/profile)](https://www.roblox.com/users/557148823/profile)</center>
+
+> A script that creates a line graph of activity in the UBA server
+
+## How does this work?
+
+1. The discord.py file will pull all the messages from the #activity channel
+2. The JSON is then broken down until it is just time stamps and the reaction count
+3. The reactions and time stamps are then put into a line graph with reactions as the Y axis and time stamp as the X axis
+4. the graph is then exported to the graph.png file
+
+Example:
+![Example Graph](./src/img/graph.png)
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from datetime import datetime
+from discord import fetchMessages
+import asyncio
+
+def createObject(data):
+    reactions = []
+    
+    for objectID in range(len(data)):
+        if "ACTIVITY CHECK" in data[objectID]["content"]:
+            reactions.append({
+                "timeStamp": data[objectID]["timestamp"],
+                "count": data[objectID]["reactions"][0]["count"]
+            })
+
+    return reactions
+
+def plotLineGraph(reactions, path=None):
+    timestamps = [datetime.fromisoformat(reactionObject["timeStamp"]) for reactionObject in reactions]
+    counts = [reactionObject["count"] for reactionObject in reactions]
+
+    plt.plot(timestamps, counts, marker='o')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Reaction Count')
+    plt.title('Activity Over Time')
+    plt.xticks(rotation=45)
+
+    if path:
+        plt.savefig(path, bbox_inches='tight')
+    else:
+        plt.show()
+
+plotLineGraph(createObject(asyncio.run(fetchMessages())), "./img/graph.png")
