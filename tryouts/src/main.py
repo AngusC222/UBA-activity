b397bd8b8248241e635ff24ed75af03028aefd84
AngusC222 <anguscrighton@icloud.com>
2024-01-30 20:02:46 +0000

    variable name fixes

--- tryouts/src/main.py
+++ tryouts/src/main.py
@@ -31,30 +31,29 @@ def createObject(data):
 def plotLineGraph(tryouts, path=None):
     timeStamps = [datetime.fromisoformat(day["timeStamp"]) for tryout in tryouts for day in tryout]
     totalAmount = [day["tryouts"]["total"] for tryout in tryouts for day in tryout]
-    GrenadierGuardsAmount = [day["tryouts"]["Grenadier Guards"] for tryout in tryouts for day in tryout]
-    RoyalMilitaryPoliceAmount = [day["tryouts"]["Royal Military Police"] for tryout in tryouts for day in tryout]
-    EducationAndTrainingServiceAmount = [day["tryouts"]["Education & Training Service"] for tryout in tryouts for day in tryout]
-    UnitedKingdomSpecialForcesAmount = [day["tryouts"]["United Kingdom Special Forces"] for tryout in tryouts for day in tryout]
-    TheParachuteRegimentAmount = [day["tryouts"]["The Parachute Regiment"] for tryout in tryouts for day in tryout]
-    RoyalGurkhaRiflesAmount = [day["tryouts"]["Royal Gurkha Rifles"] for tryout in tryouts for day in tryout]
-    RoyalArmyMedicsAmount = [day["tryouts"]["Royal Army Medics"] for tryout in tryouts for day in tryout]
+    grenadierGuards = [day["tryouts"]["Grenadier Guards"] for tryout in tryouts for day in tryout]
+    royalMilitaryPolice = [day["tryouts"]["Royal Military Police"] for tryout in tryouts for day in tryout]
+    educationAndTrainingService = [day["tryouts"]["Education & Training Service"] for tryout in tryouts for day in tryout]
+    unitedKingdomSpecialForces = [day["tryouts"]["United Kingdom Special Forces"] for tryout in tryouts for day in tryout]
+    theParachuteRegiment = [day["tryouts"]["The Parachute Regiment"] for tryout in tryouts for day in tryout]
+    royalGurkhaRifles = [day["tryouts"]["Royal Gurkha Rifles"] for tryout in tryouts for day in tryout]
+    royalArmyMedics = [day["tryouts"]["Royal Army Medics"] for tryout in tryouts for day in tryout]
 
     plt.figure(figsize=(16, 9))
 
     plt.plot(timeStamps, totalAmount, marker='o', label='Total')
     # colours may not match regiments
-    plt.plot(timeStamps, EducationAndTrainingServiceAmount, marker='o', label='Education & Training Service', color='darkblue')
-    plt.plot(timeStamps, UnitedKingdomSpecialForcesAmount, marker='o', label='United Kingdom Special Forces', color='black')
-    plt.plot(timeStamps, RoyalMilitaryPoliceAmount, marker='o', label='Royal Military Police', color='red')
-    plt.plot(timeStamps, TheParachuteRegimentAmount, marker='o', label='The Parachute Regiment', color='brown')
-    plt.plot(timeStamps, RoyalGurkhaRiflesAmount, marker='o', label='Royal Gurkha Rifles', color='indigo')
-    plt.plot(timeStamps, RoyalArmyMedicsAmount, marker='o', label='Royal Army Medics', color='lightcoral')
-    plt.plot(timeStamps, GrenadierGuardsAmount, marker='o', label='Grenadier Guards', color='gold')
+    plt.plot(timeStamps, educationAndTrainingService, marker='o', label='Education & Training Service', color='darkblue')
+    plt.plot(timeStamps, unitedKingdomSpecialForces, marker='o', label='United Kingdom Special Forces', color='black')
+    plt.plot(timeStamps, royalMilitaryPolice, marker='o', label='Royal Military Police', color='red')
+    plt.plot(timeStamps, theParachuteRegiment, marker='o', label='The Parachute Regiment', color='brown')
+    plt.plot(timeStamps, royalGurkhaRifles, marker='o', label='Royal Gurkha Rifles', color='slateGrey')
+    plt.plot(timeStamps, royalArmyMedics, marker='o', label='Royal Army Medics', color='lightcoral')
+    plt.plot(timeStamps, grenadierGuards, marker='o', label='Grenadier Guards', color='gold')
 
     plt.xlabel('Timestamp')
     plt.ylabel('Tryouts hosted')
     plt.title('Daily Tryouts Hosted Over Time')
-    # plt.xticks(rotation=45)
     plt.legend()
 
     if path:
